d907149606a1ac5efe503c0c78d678fdf391a5b0
Logan Smith <lsmith@singlestoneconsulting.com>
2023-03-04 15:53:22 -0600

    getting rid of

--- Open_API.py
+++ Open_API.py
@@ -1,27 +1,27 @@
 import openai
 import re
 import os
-#import load_dotenv from dotenv
+#import
 import requests
 
 #load_dotenv()
 
 # Set up the OpenAI API credentials
-openai.api_key = os.getenv("key")
+openai.api_key = "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"
 
 # Define a function to translate text
-"""
+
 def translate_text(input_text):
     # Define the prompt for the API request
-    prompt = f"Translate the following scientific text into simple terms:\n\n{input_text}\n\nTranslation:"
+    prompt = f"Explain the following scientific concept as if I were a kindergartener:\n\n{input_text}\n\nTranslation:"
     # Send the prompt to the API and get the response
     response = openai.Completion.create(
-        engine="davinci",
+        engine="text-davinci-003",
         prompt=prompt,
-        max_tokens=1024,
+        max_tokens=4000,
         n=1,
         stop=None,
-        temperature=0.7,
+        temperature=1,
     )
     # Extract the translated text from the response
     translated_text = response.choices[0].text
@@ -31,28 +31,3 @@ def translate_text(input_text):
     translated_text = re.sub(' +', ' ', translated_text)
     # Return the translated text
     return translated_text
-    """
-
-
-def api_test():
-    # Set up the API endpoint URL and your API key
-    url = "https://api.openai.com/v1/engine/chat/generic"
-    api_key = openai.api_key
-
-    # Set up the request headers with the API key
-    headers = {
-        "Content-Type": "application/json",
-        "Authorization": f"Bearer {api_key}",
-    }
-
-    # Set up the request body with the message you want to generate a response for
-    data = {
-        "prompt": "Hello, how are you?",
-        "temperature": 0.5,
-        "max_tokens": 16,
-    }
-
-    # Make the API request and print the response
-    response = requests.post(url, headers=headers, json=data)
-    print(response.json())
-
