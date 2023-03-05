727807efdf45def0da1f2ebebb80f43291f6e6ff
Michael Kelly <cybernationnews@gmail.com>
2023-03-05 01:39:05 -0600

    Add customization options to widget function. Add ability to search for file via frontend.

--- F_E.py
+++ F_E.py
@@ -1,5 +1,6 @@
 import tkinter as tk
 from tkinter import ttk
+from tkinter.filedialog import askopenfilename
 
 import Open_API
 
@@ -13,7 +14,10 @@ class optionFrame:
         label1_2 = tk.Label(frame, text=Open_API.generate_text_on_topic(text_entry.get(), dropdown_entry.get()))
         label1_2.pack(pady=10)
 
-    def make_tab(self, tab_name, widget_text, options=None):
+    def get_file(self):
+        filename = askopenfilename()
+
+    def make_tab(self, tab_name, widget_text, options=None, need_input=False, need_upload=False):
         # Create a frame for the second tab
         frame = tk.Frame(self.notebook)
         self.notebook.add(frame, text=tab_name)
@@ -22,9 +26,10 @@ class optionFrame:
         label = tk.Label(frame, text=widget_text)
         label.pack(pady=10)
 
-        user_entry = tk.StringVar(frame)
-        entry = tk.Entry(frame, textvariable=user_entry)
-        entry.pack()
+        if need_input:
+            user_entry = tk.StringVar(frame)
+            entry = tk.Entry(frame, textvariable=user_entry)
+            entry.pack()
 
         if options is not None:
             dropdown_text = tk.StringVar(frame)
@@ -32,10 +37,13 @@ class optionFrame:
             dropdown = tk.OptionMenu(frame, dropdown_text, *options)
             dropdown.pack()
 
-        button1 = tk.Button(frame, text="Lets go!", command=lambda: self.show_output(user_entry,
-                                                                                     dropdown_text, frame))
-        button1.pack(pady=10)
+        if need_upload:
+            upload_button = tk.Button(frame, text="Open File", command=self.get_file)
+            upload_button.pack()
 
+        button = tk.Button(frame, text="Lets go!", command=lambda: self.show_output(user_entry,
+                                                                                     dropdown_text, frame))
+        button.pack(pady=10)
 
     def construct(self):
         self.notebook = ttk.Notebook(self.root)
@@ -47,11 +55,11 @@ class optionFrame:
             "in detail with definitions.",
             "to me like I'm a kindergartener."
         ]
-        self.make_tab("Generate Information", "Explain", options)
-        self.make_tab("Generate Social Media Post", "This is Tab 2!")
-        self.make_tab("Tab 3", "This is Tab 2!")
-
-
+        self.make_tab("Generate Information", "Explain", options, need_input=True)
+        self.make_tab("Generate Social Media Post", "Write me a post about", need_input=True)
+        self.make_tab("Convert to Social Media Post", "Convert document to a post for", need_upload=True)
+        self.make_tab("Summarize Paper", "Recap the contents of", need_upload=True)
+        self.make_tab("Key Terms in Paper", "Find and define key terms in", need_upload=True)
 
 root = tk.Tk()
 
